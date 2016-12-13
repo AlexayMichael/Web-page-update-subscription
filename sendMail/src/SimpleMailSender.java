c6c82cd1abcc3773dd2c9e60b72f20496dd31d88
Alex <1140310221@hit.edu.cn>
2016-12-13 17:05:52 +0800

    邮件发送器

---
+++ sendMail/src/SimpleMailSender.java
@@ -0,0 +1,22 @@
+package sendMail;
+
+import java.util.Date;   
+import java.util.Properties;  
+import javax.mail.Address;   
+import javax.mail.BodyPart;   
+import javax.mail.Message;   
+import javax.mail.MessagingException;   
+import javax.mail.Multipart;   
+import javax.mail.Session;   
+import javax.mail.Transport;   
+import javax.mail.internet.InternetAddress;   
+import javax.mail.internet.MimeBodyPart;   
+import javax.mail.internet.MimeMessage;   
+import javax.mail.internet.MimeMultipart;   
+  
+/**  
+* 简单邮件（不带附件的邮件）发送器  
+*/   
+public class SimpleMailSender  {   
+
+}
