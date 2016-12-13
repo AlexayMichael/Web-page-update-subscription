c73f7454780728df30d4e1f09d8212a789c329b0
Alex <1140310221@hit.edu.cn>
2016-12-13 18:29:17 +0800

    实际发送

--- sendMail/src/MailAction.java
+++ sendMail/src/MailAction.java
@@ -8,13 +8,16 @@ public class MailAction {
      mailInfo.setMailServerHost("smtp.yeah.net");   
      mailInfo.setMailServerPort("25");   
      mailInfo.setValidate(true);   
+     
      mailInfo.setUserName("*****@yeah.net");   
      mailInfo.setPassword("******");//您的邮箱密码   
      mailInfo.setFromAddress("*****@yeah.net");   
      mailInfo.setToAddress("****@qq.com");   
+     
      mailInfo.setSubject("你好！");//邮件主题  
      mailInfo.setContent("这是一个测试");//邮件内容
-        //这个类主要来发送邮件  
+        
+     //这个类主要来发送邮件  
       SimpleMailSender sms = new SimpleMailSender();  
       sms.sendTextMail(mailInfo);//发送文体格式   
       sms.sendHtmlMail(mailInfo);//发送html格式 
